3710f272ffdf26601c9795b977d8371e8feb9555
nimasratna <nimasratna@gmail.com>
2021-03-09 00:43:41 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'Index Page'
+
+@app.route('/hello')
+def hello():
+  return 'Hello, greetings from different endpoint'
+
+#adding variables
+@app.route('/user/<username>')
+def show_user(username):
+  #returns the username
+  return 'Username: %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+  #returns the post, the post_id should be an int
+  return str(post_id)
