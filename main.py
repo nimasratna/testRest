cdc2e378ccd6df78877b9927402b567ce363631c
nimasratna <nimasratna@gmail.com>
2021-03-19 10:02:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -106,5 +106,5 @@ def sendTemplateEmail(source_email, email_to, cc, bcc, template_name, TemplateDa
     #create_template()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
     
